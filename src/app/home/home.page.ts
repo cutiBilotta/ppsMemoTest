f16f4534d09f48069133ffc5de8a35cf4c2a932d
cutiBilotta <agus.bilotta@gmail.com>
2024-05-07 18:34:13 -0300

    commit martes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit  {
 
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,  private afAuth : AngularFireAuth) {}
+  public userEmail : string|null=null;
 
+  ngOnInit(): void {
+    this.afAuth.authState.subscribe(user => {
+      if (user) {
+        this.userEmail = user.email;
+      } else {
+        console.log("null");
+      }
+    });
+  }
 
+  cerrarSesion(){
+
+    this.afAuth.signOut();
+    this.router.navigateByUrl('login');
+  }
+  
   seleccionarDificultad(seleccion :string){
 
 
--- src/app/juego/juego.page.ts
+++ src/app/juego/juego.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChildren, ElementRef, QueryList } from '@angular/core';
 import { NavigationExtras, Router, ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2'
 
 
 @Component({
@@ -9,30 +10,70 @@ import { NavigationExtras, Router, ActivatedRoute } from '@angular/router';
 })
 export class JuegoPage implements OnInit {
 
-  constructor(private router: ActivatedRoute) {}
+  @ViewChildren('imagenElemento') imagenElementos!: QueryList<ElementRef>;
+ 
+  constructor(private router: ActivatedRoute, public route : Router) {}
 
   public dificultad : string ="";
   public arrayImagenes: string[] = [];
   private frutas: string[] = ['arandano', 'anana', 'multi', 'cereza' , 'manzana', 'banana', 'naranja', 'pera','arandano', 'anana', 'multi', 'cereza' , 'manzana', 'banana', 'naranja', 'pera'];
   private animales: string[] = ['flamenco', 'pinguino', 'tortuga','flamenco', 'pinguino', 'tortuga'];
   private herramientas: string[] = ['lapiz', 'engranaje', 'serrucho', 'tijera' , 'herramientas', 'lapiz', 'engranaje', 'serrucho', 'tijera' , 'herramientas'];
-
+  private paresAcertados =0;
   public seleccionRonda: number =0;
 
+  private cronometro:any;
   public primeraImagen :string = "";
   public segundaImagen :string = "";
 
-  bloquearImagen: { [nombre: string]: boolean } = {};
+  public segundos = 0;
+  public minutos = 0;
+  public indiceAnterior : number=-1;
+  elementosImg: HTMLImageElement[] = [];
+
 
 
   ngOnInit() {
-
     this.router.queryParams.subscribe(params => {
       this.dificultad = params['dato'];
     });
 
     this.seleccionarImagenesAleatorias();
+  }
 
+  ngAfterViewInit() {
+    this.imagenElementos.forEach((elemento: ElementRef) => {
+      this.elementosImg.push(elemento.nativeElement as HTMLImageElement);
+    });
+  
+    console.log(this.elementosImg);
+  
+    Swal.fire({
+      html:
+        '<br><label style="font-size:80%">Estás listo para comenzar?</label>',
+      confirmButtonText: "Siii",
+      confirmButtonColor: 'var(--ion-color-primary)',
+      heightAuto: false
+  
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.iniciarCronometro();
+      }
+    });
+  }
+  
+  iniciarCronometro() {
+
+    this.cronometro = setInterval(() => {
+      this.segundos++;
+      if (this.segundos === 60) {
+        this.minutos++;
+        this.segundos = 0;
+      }
+      console.log(`${this.minutos}:${this.segundos}`);
+    }, 1000); // se ejecuta cada segundo
+  
+    // Si necesitas detener el cronómetro en algún momento, puedes hacerlo usando clearInterval(cronometro);
   }
 
 
@@ -77,28 +118,50 @@ export class JuegoPage implements OnInit {
   }
   
      
-  seleccionarImagen(nombreImagen: string) {
+  seleccionarImagen(nombreImagen: string, indice: number) {
     this.seleccionRonda++;
-  
-    if (this.seleccionRonda === 1) {
-      this.primeraImagen = nombreImagen;
-      console.log(this.seleccionRonda + " - " + nombreImagen);
-      this.bloquearImagen[nombreImagen] = true; // Bloquear la imagen seleccionada
-    } else if (this.seleccionRonda === 2) {
-      this.segundaImagen = nombreImagen;
-      console.log(this.seleccionRonda + " - " + nombreImagen);
-  
-      if (this.primeraImagen === this.segundaImagen) {
-        console.log("acertaste");
-      } else {
-        console.log("perdiste! siguiente ronda");
-      }
-      this.seleccionRonda = 0;
-    }
-  }
 
-  }
+    this.elementosImg[indice].src = `../../assets/img/${this.dificultad}/${nombreImagen}.png`, innerHeight=100 , innerWidth=100 ;
+
+    if (this.seleccionRonda === 1) {
+        this.primeraImagen = nombreImagen;
+        this.indiceAnterior = indice;
+    } else if (this.seleccionRonda === 2) {
+        this.segundaImagen = nombreImagen;
+
+        if (this.primeraImagen === this.segundaImagen) {
+            console.log("acertaste");
+            this.paresAcertados ++;
+        } else {
+            setTimeout(() => {
+                this.elementosImg[indice].src = `../../assets/img/q.png`;
+                this.elementosImg[this.indiceAnterior].src = `../../assets/img/q.png`;
+                console.log("perdiste! siguiente ronda");
+            }, 1000); // 2000 milisegundos = 2 segundos
+        }
+
+        this.seleccionRonda = 0;
+    }
+
+    if(this.paresAcertados== this.arrayImagenes.length/2){
+
+      clearInterval(this.cronometro);
+      Swal.fire({
+        html: `<br><label style="font-size:80%">Has ganado el juego!</label><br><label style="font-size:80%">En ${this.minutos}:${this.segundos}</label>`,
+        confirmButtonText: "Perfecto!",
+        confirmButtonColor: 'var(--ion-color-primary)',
+        heightAuto: false
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.route.navigateByUrl('home');
+        }
+      });
+
+    }
+}
+
   
+}
 
 
 
